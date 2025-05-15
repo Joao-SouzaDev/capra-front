9f603a920a66d90adca9d7f64871ebead87a67c4
Joao-SouzaDev <joao.souzadev@outlook.com>
2025-05-15 12:59:37 -0300

    Componentizado Dialog de login, e adicionado gatilhos.

--- public/js/header.js
+++ public/js/header.js
@@ -1,9 +1,10 @@
-function carregaHeader() {
-    fetch('components/header.html')
+async function carregaHeader() {
+    await fetch('components/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header').innerHTML = data;
         })
         .catch(error => console.error('Error loading header:', error));
-}
+};
 document.addEventListener("DOMContentLoaded", carregaHeader);
+
