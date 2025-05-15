ed29a95b41dd112a1d6dfd3055687cbd591e85eb
Joao-SouzaDev <joao.souzadev@outlook.com>
2025-05-14 21:04:38 -0300

    Adicionado api de renderizacao e criado componentizacao de header

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+// Servir arquivos estáticos (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rota para a página inicial
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/animais', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'pages/listagemAnimais.html'));
+});
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
